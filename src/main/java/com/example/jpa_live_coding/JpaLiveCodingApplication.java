00de6febe8b0be89b17af2b982b29f3079f1f38a
bluewow <dogseen@gmail.com>
2020-07-28 07:09:04 +0900

    init

---
+++ src/main/java/com/example/jpa_live_coding/JpaLiveCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_live_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLiveCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLiveCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa_live_coding/JpaLiveCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_live_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLiveCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
